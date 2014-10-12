988275d787af39d07ed91ad3d8dbff0016eb6c4d
Anatoly Mischenkov <anatoly.mischenkov@gmail.com>
2014-10-13 02:35:25 +0400

    Initial Commit

---
+++ Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storyboards
+//
+//  Created by Mischenkov Anatoly on 13/10/14.
+//  Copyright (c) 2014 mischenkov.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
